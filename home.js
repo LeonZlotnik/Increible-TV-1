27018032cc5596f8eda040c6a15a3e103b06628b
Gerardo <gerardosgv@gmail.com>
2018-01-07 19:42:37 -0600

    Increibletv

---
+++ home.js
@@ -0,0 +1,11 @@
+var slider = document.getElementById('slider');
+
+var sliderArray = []:
+
+var scrollable = document.querySelector("scrollable");
+
+var description = document.querySelector("description");
+
+scrollable.onmouseover = function(){
+  description.add("visible");
+}
